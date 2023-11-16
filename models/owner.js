6a86e4b3b006fe670767a99ded3e21fea01bbf9e
KPP3 <137243573+KPP3@users.noreply.github.com>
2023-11-15 18:10:28 -0600

    final commit

---
+++ models/owner.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+var propertySchema = new mongoose.Schema(
+  {
+    address: String,
+    moveInDate: Date,
+    bedroom: Number,
+    bathroom: Number,
+    utility: String,
+    furnish: String,
+    parking: Number,
+    rent: Number,
+    image: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+var ownerSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    avatar: String,
+    properties: [propertySchema],
+    googleId: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Owner", ownerSchema);
---
+++ server.js
@@ -0,0 +1,80 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// require express-session: the use of express-session is server-side way 
+// remebering browser session
+var session = require('express-session')
+// require Passport: the use of passport is to authenticate user
+// as the passport has different strategies which do most of the heavy-lifting
+// for us to authenticate
+var passport = require('passport')
+var methodOverride = require('method-override');
+
+// load the env vars
+require('dotenv').config();
+
+var indexRouter = require('./routes/index');
+var ownersRouter = require('./routes/owners');
+
+var app = express();
+
+// connect to the MongoDB with mongoose when app starts
+require('./config/database');
+// connect to the passport module
+require('./config/passport')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// configure and mount session middleware
+app.use(session({
+  secret: 'YourAccommodation!',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  if(req.query._method == 'DELETE') {
+    req.method = 'DELETE'
+    req.url = req.path
+  }
+  next();
+})
+app.use(function(req, res, next) {
+  if(req.query._method == 'PUT') {
+    req.method = 'PUT'
+    req.url = req.path
+  }
+  next();
+})
+
+app.use('/', indexRouter);
+app.use('/', ownersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
